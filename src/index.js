66e423bc66cc5e2a1605b5f08b5e160efbcaaf78
nouali <mohamedngmail.com>
2023-11-25 17:25:57 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <BrowserRouter basename='/CC1_react'>
+    <BrowserRouter basename='/navbar'>
     <App />
     </BrowserRouter>
     
